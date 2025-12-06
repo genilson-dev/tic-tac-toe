1d2ddc957edef7b9591a1384e23c67daff8819d6
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-12-06 20:58:58 -0300

    feat: Adicionei os loop for para gerar os arrays

--- script/jogo-velha.js
+++ script/jogo-velha.js
@@ -1,7 +1,31 @@
-function start(){}
+let tabuleiro;
+let board;
+let aviso;
+let jogador;
+let linha;
+let coluna;
 
-function exibir(){}
 
-function jogar(){}
+function start() {
+    tabuleiro = [];
+    board = document.getElementById("board")
+    aviso = document.getElementById("aviso")
+    jogador = 1;
+    for(let i = 0; i < 3; i++){
+        tabuleiro[i] = [];
+        // console.log(tabuleiro);
+        for(let t = 0; t < 3; t++){
+            tabuleiro[i][t] = 0
+            // console.log(tabuleiro);
+            
+        }
+    }
+    console.table(tabuleiro);   
+    exibir()     
+}
 
-function checar(){}
+function exibir() { }
+
+function jogar() { }
+
+function checar() { }
